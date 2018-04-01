5518eb7dced134c4c988a10874a5747d4f0dd8b4
Sakitama <3278889655@qq.com>
2018-04-02 00:15:14 +0800

    add compose.js

---
+++ compose.js
@@ -0,0 +1,22 @@
+/**
+ * Composes single-argument functions from right to left. The rightmost
+ * function can take multiple arguments as it provides the signature for
+ * the resulting composite function.
+ *
+ * @param {...Function} funcs The functions to compose.
+ * @returns {Function} A function obtained by composing the argument functions
+ * from right to left. For example, compose(f, g, h) is identical to doing
+ * (...args) => f(g(h(...args))).
+ */
+
+export default function compose(...funcs) { // 接受一组函数参数，从右往左组合多个函数，然后返回一个组合函数
+    if (funcs.length === 0) {
+        return arg => arg
+    }
+
+    if (funcs.length === 1) {
+        return funcs[0]
+    }
+
+    return funcs.reduce((a, b) => (...args) => a(b(...args)))
+}
